77fa22307d6a8a07cd068b9dde7617469fe90d1a
Hanvu2k <vungochan1410l@gmail.com>
2022-10-09 23:12:18 +0700

    get users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,6 +19,17 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUser = async (req, res) => {
+    let id = req.body.id
+    let users = await userService.getAllUser(id)
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'ok',
+        users
+    })
+}
+
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUser: handleGetAllUser,
 }
