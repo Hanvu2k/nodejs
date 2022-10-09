ed51b4cfc6cad45f8c7328f8a9cb29a0d69a5480
Hanvu2k <vungochan1410l@gmail.com>
2022-10-09 22:41:02 +0700

    add cors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,7 +14,7 @@ let handleLogin = async (req, res) => {
     let userData = await userService.handleUserLogin(email, password)
     return res.status(200).json({
         errCode: userData.errCode,
-        message: userData.errmessage,
+        message: userData.errMessage,
         user: userData.user ? userData.user : {},
     })
 }
--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ require('dotenv').config()
 
 
 let app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Request-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
